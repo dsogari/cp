54d7b345c46975edbc1341c7c6adf2be24553d7e
Diego Sogari <diego.sogari@gmail.com>
2024-05-03 16:40:21 -0300

    Use C++

--- rounds/943/prefiquence/prefiquence.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { prefiquence } from "./prefiquence";
-
-describe("prefiquence", () => {
-  it("should pass", () => {
-    expect(prefiquence('10011', '1110')).toEqual(2);
-  });
-});
--- rounds/943/prefiquence/prefiquence.ts
+++
@@ -1,9 +0,0 @@
-export function prefiquence(a: string, b: string): number {
-  let k = 0;
-  for (let j = 0; k < a.length && j < b.length; ++j) {
-    if (a[k] == b[j]) {
-      ++k;
-    }
-  }
-  return k;
-}

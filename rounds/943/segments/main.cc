8236a3375fdcc786948875b61da79b1c536a639a
Diego Sogari <diego.sogari@gmail.com>
2024-05-04 12:41:29 -0300

    [943] Add segments

---
+++ rounds/943/segments/main.cc
@@ -0,0 +1,44 @@
+#include "utils.h"
+
+IO(__FILE__);
+
+void run(int t)
+{
+  int n, q;
+  in >> n >> q;
+  vector<int> b(n + 1);
+  for (int i = 1; i <= n; ++i)
+  {
+    in >> b[i];
+    b[i] ^= b[i - 1];
+  }
+  map<int, vector<int>> p;
+  for (int i = 0; i <= n; ++i)
+    p[b[i]].push_back(i);
+  const auto f = [&b, &p](int l, int r)
+  {
+    const auto bl = b[l - 1];
+    const auto br = b[r];
+    if (bl == br)
+      return true;
+    const auto &pl = p[bl];
+    const auto &pr = p[br];
+    const auto s = ranges::lower_bound(pr, l);
+    const auto t = ranges::lower_bound(pl, r);
+    return s < pr.end() && t > pl.begin() && *s < *prev(t);
+  };
+  for (int l, r; q; --q)
+  {
+    in >> l >> r;
+    out << (f(l, r) ? "YES" : "NO") << endl;
+  }
+  out << endl;
+}
+
+int main()
+{
+  int t;
+  in >> t;
+  for (int i = 1; i <= t; ++i)
+    run(i);
+}
--- rounds/utils.h
+++ rounds/utils.h
@@ -7,11 +7,11 @@ using i64 = int64_t;
 
 #ifdef DEBUG
 using filesystem::path;
-#define IO(file) \
+#define IO(file)                                                \
   auto in = ifstream(path(file).replace_filename("input.txt")); \
   auto &out = cout << fixed << setprecision(20)
 #else
-#define IO(_) \
+#define IO(_)                                                 \
   auto &in = (cin.tie(nullptr)->sync_with_stdio(false), cin); \
   auto &out = cout << fixed << setprecision(20)
 #endif

34a0a0f4b6d442ce602d72840b25f5f575b39f74
Diego Sogari <diego.sogari@gmail.com>
2024-08-30 00:46:13 -0300

    [2003] Solve problem E2

--- codeforces/2003/e2/main.cc
+++ codeforces/2003/e2/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2003/submission/278757503
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -23,7 +25,66 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+struct Digraph : vector<vector<int>> {
+  const int n, m;
+  Digraph(int n, int m = 0) : vector<vector<int>>(n + 1), n(n), m(m) {
+    for (auto &[u, v] : vector<array<Int, 2>>(m)) {
+      add(u, v);
+    }
+  }
+  void add(int u, int v) { (*this)[u].push_back(v); }
+};
+
+constexpr int zero = 1, one = 2;
+
+void solve(int t) { // O(n + m)
+  Int n, m;
+  Digraph g(n, m);
+  vector<int> a(n);
+  for (int l = 1, L = 0, R1 = 0, R2 = 0; l <= n; l++) { // O(n + m)
+    if (g[l].empty()) {
+      continue; // no interval starts here
+    }
+    int mn = n, mx = 0;
+    for (auto &r : g[l]) {
+      mn = min(mn, r);
+      mx = max(mx, r);
+    }
+    if (l > R2) {
+      a[l - 1] = zero;
+      for (int j = mn; j <= mx; j++) {
+        a[j - 1] = one;
+      }
+      L = l, R1 = mn, R2 = mx;
+      continue; // non-overlapping interval
+    }
+    for (int j = L + 1; j <= l; j++) {
+      if (a[j - 1] == one) {
+        println(-1);
+        return; // no interesting permutation exists
+      }
+      a[j - 1] = zero;
+    }
+    for (int j = mn; j < R1; j++) {
+      a[j - 1] = one;
+    }
+    for (int j = R2 + 1; j <= mx; j++) {
+      a[j - 1] = one;
+    }
+    L = l, R1 = min(R1, mn), R2 = max(R2, mx);
+  }
+  int ans = n * (n - 1) / 2, zeros = 0, ones = 0;
+  for (int l = 0, r = n - 1; l <= r;) { // O(n)
+    if (zeros < ones) {
+      a[l] == zero ? zeros++ : ans -= zeros;
+      l++;
+    } else {
+      a[r] == one ? ones++ : ans -= ones;
+      r--;
+    }
+  }
+  println(ans - zeros * ones);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);
--- codeforces/2003/f/main.cc
+++ codeforces/2003/f/main.cc
@@ -1,9 +1,12 @@
 /**
+ * https://codeforces.com/contest/2003/submission/278736438
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using u64 = uint64_t;
 
 #ifdef ONLINE_JUDGE
 #define debug
@@ -23,11 +26,20 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+void solve(int t) {
+  Int n, m;
+  vector<Int> a(n), b(n), c(n);
+  int ans = 0;
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);
+#ifdef ONLINE_JUDGE
+  int t = 1;
+#else
   Int t;
+#endif
   for (int i = 1; i <= t; i++) {
     solve(i);
   }

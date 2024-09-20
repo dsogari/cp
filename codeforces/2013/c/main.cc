7a16a140c4a7689e3e60c784c78e1a9ef79a72f0
Diego Sogari <diego.sogari@gmail.com>
2024-09-20 12:56:52 -0300

    [2013] Solve problem C

--- codeforces/2013/c/main.cc
+++ codeforces/2013/c/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2013/submission/282080555
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -23,7 +25,65 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+struct Str : string {
+  Str() { cin >> *this; }
+};
+
+int simulate(const string &pass, const string &s) {
+  return pass.find(s) != string::npos;
+}
+
+mt19937 rng(random_device{}());
+
+void solve(int t) {
+  Int n;
+#ifndef ONLINE_JUDGE
+  Str pass;
+#endif
+  int rem = 2 * n;
+  auto query = [&](const string &s) {
+    assert(rem--);
+    println('?', s);
+#ifndef ONLINE_JUDGE
+    return simulate(pass, s);
+#else
+    return Int();
+#endif
+  };
+  string ans;
+  bool right = false;
+  while (ans.size() < n) {
+    if (!right) {
+      auto r = query(ans + "0");
+      if (r < 0) {
+        return;
+      }
+      if (r) {
+        ans += "0";
+        continue;
+      }
+      r = query(ans + "1");
+      if (r < 0) {
+        return;
+      }
+      if (r) {
+        ans += "1";
+        continue;
+      }
+      right = true;
+    }
+    auto r = query("0" + ans);
+    if (r < 0) {
+      return;
+    }
+    if (r) {
+      ans = "0" + ans;
+      continue;
+    }
+    ans = "1" + ans;
+  }
+  println('!', ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

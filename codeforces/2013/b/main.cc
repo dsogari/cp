18e4b340fedc7db88aa6c5f454d7e730b209092d
Diego Sogari <diego.sogari@gmail.com>
2024-09-20 11:55:49 -0300

    [2013] Solve problem B

--- codeforces/2013/b/main.cc
+++ codeforces/2013/b/main.cc
@@ -1,9 +1,12 @@
 /**
+ * https://codeforces.com/contest/2013/submission/282032335
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = int64_t;
 
 #ifdef ONLINE_JUDGE
 #define debug
@@ -23,7 +26,15 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+void solve(int t) {
+  Int n;
+  vector<Int> a(n);
+  i64 ans = a[n - 1] - a[n - 2];
+  for (int i = 0; i < n - 2; i++) {
+    ans += a[i];
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

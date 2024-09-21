1ffcd5cdad92f6ec9e7a11bb57571dafb7c0642c
Diego Sogari <diego.sogari@gmail.com>
2024-09-21 07:33:28 -0300

    [2013] Solve problem F1

--- codeforces/2013/f1/main.cc
+++ codeforces/2013/f1/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2013/submission/282194464
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -23,7 +25,42 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+struct Graph : vector<vector<int>> {
+  const int n, m;
+  Graph(int n, int m = 0) : vector<vector<int>>(n + 1), n(n), m(m) {
+    for (auto &[u, v] : vector<array<Int, 2>>(m)) {
+      add(u, v);
+    }
+  }
+  void add(int u, int v) { _add(u, v), _add(v, u); }
+  void _add(int u, int v) { (*this)[u].push_back(v); }
+};
+
+void solve(int t) {
+  Int n;
+  Graph g(n, n - 1);
+  Int u, v;
+  assert(u == v);
+  vector<bool> vis(n + 1);
+  auto f = [&](auto &self, int a, int b, bool turn) -> bool {
+    bool ans = turn;
+    for (auto &&v : g[turn ? b : a]) {
+      if (!vis[v]) {
+        vis[v] = true;
+        auto alicewins = self(self, turn ? a : v, turn ? v : b, !turn);
+        vis[v] = false;
+        if (turn ^ alicewins) {
+          ans = !ans;
+          break;
+        }
+      }
+    }
+    return ans;
+  };
+  vis[1] = vis[u] = true;
+  auto ans = f(f, 1, u, 0) ? "Alice" : "Bob";
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

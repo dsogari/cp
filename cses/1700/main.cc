ac4ba4ce9e81235880c199cf6eaccc06d596e867
Diego Sogari <diego.sogari@gmail.com>
2024-09-27 13:24:20 -0300

    [CSES] Solve problem 1700

---
+++ cses/1700/main.cc
@@ -0,0 +1,73 @@
+/**
+ * https://cses.fi/problemset/result/10648518/
+ *
+ * (c) 2024 Diego Sogari
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using u64 = uint64_t;
+
+#ifdef ONLINE_JUDGE
+#define debug
+#else
+#include "debug.h"
+init();
+#endif
+
+void println(auto &&...args) { ((cout << args << ' '), ...) << endl; }
+
+template <typename T> struct Num {
+  T x;
+  Num() { cin >> x; }
+  Num(T a) : x(a) {}
+  operator T &() { return x; }
+  operator T() const { return x; }
+};
+using Int = Num<int>;
+
+struct Graph : vector<vector<int>> {
+  const int n, m;
+  Graph(int n, int m = 0) : vector<vector<int>>(n + 1), n(n), m(m) {
+    for (auto &[u, v] : vector<array<Int, 2>>(m)) {
+      add(u, v);
+    }
+  }
+  void add(int u, int v) { _add(u, v), _add(v, u); }
+  void _add(int u, int v) { (*this)[u].push_back(v); }
+};
+
+struct SplitHash {
+  size_t operator()(u64 x) const {
+    x += (u64)this + 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+};
+
+void solve(int t) {
+  Int n;
+  Graph g1(n, n - 1), g2(n, n - 1);
+  SplitHash hash;
+  unordered_map<u64, int> hashes = {{0, 0}};
+  auto f = [&](auto &self, auto &g, int u, int p) -> int { // O(n)
+    u64 x = 0;
+    for (auto &&v : g[u]) {
+      if (v != p) {
+        x += hash(self(self, g, v, u));
+      }
+    }
+    return hashes.emplace(x, hashes.size() + 1).first->second;
+  };
+  auto ans = f(f, g1, 1, 1) == f(f, g2, 1, 1) ? "YES" : "NO";
+  println(ans);
+}
+
+int main() {
+  cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);
+  Int t;
+  for (int i = 1; i <= t; i++) {
+    solve(i);
+  }
+}

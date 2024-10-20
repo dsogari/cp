7c1ba9c3cad19372638e3a118cd4372c21dfc703
Diego Sogari <diego.sogari@gmail.com>
2024-10-20 15:55:44 -0300

    [2030] Solve problem E

--- codeforces/2030/e/main.cc
+++ codeforces/2030/e/main.cc
@@ -1,9 +1,13 @@
 /**
+ * https://codeforces.com/contest/2030/submission/287112231
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using u64 = uint64_t;
+using u128 = __uint128_t; // available on 64-bit targets
 
 #ifdef ONLINE_JUDGE
 #define debug(...)
@@ -23,7 +27,95 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+template <typename T, auto M> struct Mod {
+  using V = conditional_t<sizeof(T) <= 4, u64, u128>;
+  static V inv(V x, V m) { return x > 1 ? m - inv(m % x, x) * m / x : 1; }
+  make_unsigned_t<T> x;
+  Mod() : x(0) {}
+  Mod(auto y) : x(y % M) { x >= M ? x += M : x; }
+  operator T() const { return x; }
+  Mod operator-() const { return Mod() -= *this; }
+  Mod operator+(auto rhs) const { return Mod(*this) += rhs; }
+  Mod operator-(auto rhs) const { return Mod(*this) -= rhs; }
+  Mod operator*(auto rhs) const { return Mod(*this) *= rhs; }
+  Mod operator/(auto rhs) const { return Mod(*this) /= rhs; }
+  Mod &operator+=(Mod rhs) { return (x += rhs.x) >= M ? x -= M : x, *this; }
+  Mod &operator-=(Mod rhs) { return (x -= rhs.x) >= M ? x += M : x, *this; }
+  Mod &operator*=(Mod rhs) { return x = x * V(rhs.x) % M, *this; }
+  Mod &operator/=(Mod rhs) { return x = x * inv(rhs.x, M) % M, *this; }
+  Mod pow(auto y) const { // O(log y) | 0^(-inf,0] -> 1
+    Mod ans(1), base(*this);
+    for (auto e = y < 0 ? ~y + u128(1) : +y; e; e >>= 1, base *= base) {
+      e & 1 ? ans *= base : ans;
+    }
+    return y < 0 ? Mod(1) /= ans : ans;
+  }
+};
+using Mint = Mod<int, 998244353>;
+
+struct Binom {
+  vector<Mint> fac, inv;
+  Binom() : fac(1, 1), inv(1, 1) {}
+  Mint permute(int n) { return update(n), fac.at(n); }               // O(1)
+  Mint commute(int n) { return update(n), inv.at(n); }               // O(1)
+  Mint invert(int n) { return arrange(n - 1, -1); }                  // O(1)
+  Mint pascald(int n, int k) { return combine(n + k, k); }           // O(1)
+  Mint catalan(int n) { return combine(2 * n, n) * invert(n + 1); }  // O(1)
+  Mint arrange(int n, int k) { return permute(n) * commute(n - k); } // O(1)
+  Mint combine(int n, int k) { return arrange(n, k) * commute(k); }  // O(1)
+  void reserve(int n) { fac.reserve(n), inv.reserve(n); }            // O(n)
+  void update(int n) { // O(1) amortized
+    int s = fac.size();
+    if (s <= n) {
+      fac.resize(n + 1);
+      inv.resize(n + 1);
+      for (int i = s; i <= n; i++) {
+        fac[i] = fac[i - 1] * i;
+      }
+      inv[n] = Mint(1) / fac[n];
+      for (int i = n; i > s; i--) {
+        inv[i - 1] = inv[i] * i;
+      }
+    }
+  }
+} binom;
+
+void solve(int t) {
+  Int n;
+  vector<Int> a(n);
+  binom.reserve(n);
+  vector<int> cnt(n);
+  for (auto &&ai : a) { // O(n)
+    cnt[ai]++;
+  }
+  vector<Mint> pow2(n + 1), dp(n + 1);
+  pow2[0] = dp[cnt[0]] = 1;
+  for (int i = 1; i <= n; i++) { // O(n)
+    pow2[i] = pow2[i - 1] * 2;
+  }
+  Mint ans;
+  for (int i = 0, mn = n, total = 0; i < n && cnt[i]; i++) { // O(n)
+    auto c = cnt[i];
+    Mint sum, acc1, acc2;
+    for (int j = c; j > mn; j--) {
+      acc1 += binom.combine(c, j);
+    }
+    for (int j = mn; j > c; j--) {
+      acc2 += dp[j];
+    }
+    mn = min(mn, c);
+    for (int j = mn; j > 0; j--) {
+      auto comb = binom.combine(c, j);
+      acc2 += dp[j];
+      dp[j] = dp[j] * acc1 + comb * acc2;
+      acc1 += comb;
+      sum += dp[j] * j;
+    }
+    total += c;
+    ans += sum * pow2[n - total];
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

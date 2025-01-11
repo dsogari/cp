df91914695f60b3395f6b591c223dd6244b9559c
Diego Sogari <diego.sogari@gmail.com>
2025-01-11 16:56:01 -0300

    [2033] Solve problem F

--- codeforces/2033/f/main.cc
+++ codeforces/2033/f/main.cc
@@ -1,9 +1,14 @@
 /**
+ * https://codeforces.com/contest/2033/submission/300562510
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u128 = __uint128_t; // available on 64-bit targets
 
 #ifdef ONLINE_JUDGE
 #define debug(...)
@@ -22,8 +27,44 @@ template <typename T> struct Num {
   operator T() const { return x; }
 };
 using Int = Num<int>;
+using I64 = Num<i64>;
 
-void solve(int t) {}
+template <typename T, auto M> struct Mod {
+  using V = conditional_t<sizeof(T) <= 4, u64, u128>;
+  static V inv(V x, V m) { return x > 1 ? m - inv(m % x, x) * m / x : 1; }
+  make_unsigned_t<T> x;
+  Mod() : x(0) {}
+  Mod(auto y) : x(y % M) { x >= M ? x += M : x; }
+  operator T() const { return x; }
+  Mod operator-() const { return Mod() -= *this; }
+  Mod operator+(auto rhs) const { return Mod(*this) += rhs; }
+  Mod operator-(auto rhs) const { return Mod(*this) -= rhs; }
+  Mod operator*(auto rhs) const { return Mod(*this) *= rhs; }
+  Mod operator/(auto rhs) const { return Mod(*this) /= rhs; }
+  Mod &operator+=(Mod rhs) { return (x += rhs.x) >= M ? x -= M : x, *this; }
+  Mod &operator-=(Mod rhs) { return (x -= rhs.x) >= M ? x += M : x, *this; }
+  Mod &operator*=(Mod rhs) { return x = x * V(rhs.x) % M, *this; }
+  Mod &operator/=(Mod rhs) { return x = x * inv(rhs.x, M) % M, *this; }
+  Mod pow(auto y) const { // O(log y) | 0^(-inf,0] -> 1
+    Mod ans(1), base(*this);
+    for (auto e = y < 0 ? ~y + u128(1) : +y; e; e >>= 1, base *= base) {
+      e & 1 ? ans *= base : ans;
+    }
+    return y < 0 ? Mod(1) /= ans : ans;
+  }
+};
+using Mint = Mod<int, 1000000007>;
+
+void solve(int t) {
+  I64 n, k;
+  int freq = 1;
+  for (int a = 0, b = 1, c = 1; c % k; a = b, b = c % k, freq++) {
+    c = a + b;
+  }
+  debug(freq);
+  Mint ans = Mint(freq) * n;
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

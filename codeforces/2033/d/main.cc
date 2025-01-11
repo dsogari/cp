d0d1f968c8a74700f080ebcc7c37cfaf91d3b359
Diego Sogari <diego.sogari@gmail.com>
2025-01-11 11:53:43 -0300

    [2033] Solve problem D

--- codeforces/2033/d/main.cc
+++ codeforces/2033/d/main.cc
@@ -1,9 +1,12 @@
 /**
+ * https://codeforces.com/contest/2033/submission/300533251
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = int64_t;
 
 #ifdef ONLINE_JUDGE
 #define debug(...)
@@ -23,7 +26,24 @@ template <typename T> struct Num {
 };
 using Int = Num<int>;
 
-void solve(int t) {}
+void solve(int t) {
+  Int n;
+  vector<Int> a(n);
+  vector<i64> pref(n + 1);
+  for (int i = 0; i < n; i++) {
+    pref[i + 1] = pref[i] + a[i];
+  }
+  set<i64> sums;
+  int ans = 0;
+  for (int i = n; i > 0; i--) {
+    sums.insert(pref[i]);
+    if (sums.contains(pref[i - 1])) {
+      sums.clear();
+      ans++;
+    }
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

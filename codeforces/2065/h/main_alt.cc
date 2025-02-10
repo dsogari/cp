304dcbebc7b19eb5fa589c2e62d34d2d14e219e0
Diego Sogari <diego.sogari@gmail.com>
2025-02-10 04:38:41 -0300

    [2065] Add alternative solution to problem H

---
+++ codeforces/2065/h/main_alt.cc
@@ -0,0 +1,144 @@
+/**
+ * https://codeforces.com/contest/2065/submission/305424914
+ *
+ * (c) 2025 Diego Sogari
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using u64 = uint64_t;
+using u128 = __uint128_t; // available on 64-bit targets
+
+#ifdef ONLINE_JUDGE
+#define debug(...)
+#else
+#include "debug.h"
+init();
+#endif
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &a) {
+  return ranges::for_each(a, [&os](auto &ai) { os << ai << ' '; }), os;
+}
+void println(auto &&...args) { ((cout << args << ' '), ...) << endl; }
+
+template <typename T> struct Num {
+  T x;
+  Num() { cin >> x; }
+  Num(T a) : x(a) {}
+  operator T &() { return x; }
+  operator T() const { return x; }
+};
+using Int = Num<int>;
+
+template <typename T> struct Str : basic_string<T> {
+  using basic_string<T>::basic_string;
+  Str() { cin >> *this; }
+};
+using String = Str<char>;
+
+template <typename T, auto M> struct Mod {
+  using V = conditional_t<sizeof(T) <= 4, u64, u128>;
+  static V inv(V x, V m) { return x > 1 ? m - inv(m % x, x) * m / x : 1; }
+  make_unsigned_t<T> x;
+  Mod() : x(0) {}
+  Mod(auto y) : x(y % M) { x >= M ? x += M : x; }
+  operator T() const { return x; }
+  Mod operator-() const { return Mod() -= *this; }
+  Mod operator+(auto rhs) const { return Mod(*this) += rhs; }
+  Mod operator-(auto rhs) const { return Mod(*this) -= rhs; }
+  Mod operator*(auto rhs) const { return Mod(*this) *= rhs; }
+  Mod operator/(auto rhs) const { return Mod(*this) /= rhs; }
+  Mod &operator+=(Mod rhs) { return (x += rhs.x) >= M ? x -= M : x, *this; }
+  Mod &operator-=(Mod rhs) { return (x -= rhs.x) >= M ? x += M : x, *this; }
+  Mod &operator*=(Mod rhs) { return x = x * V(rhs.x) % M, *this; }
+  Mod &operator/=(Mod rhs) { return x = x * inv(rhs.x, M) % M, *this; }
+  Mod pow(auto y) const { // O(log y) | 0^(-inf,0] -> 1
+    Mod ans(1), base(*this);
+    for (auto e = y < 0 ? ~y + u128(1) : +y; e; e >>= 1, base *= base) {
+      e & 1 ? ans *= base : ans;
+    }
+    return y < 0 ? Mod(1) /= ans : ans;
+  }
+};
+using Mint = Mod<int, 998244353>;
+
+struct Pow2 : vector<Mint> {
+  Pow2(int n) : vector<Mint>(n + 1, 1) { // O(n)
+    for (int i = 0; i < n; i++) {
+      (*this)[i + 1] = (*this)[i] * 2;
+    }
+  }
+};
+
+template <typename T> struct SegTree {
+  int n;
+  vector<T> nodes;
+  function<T(const T &, const T &)> f;
+  SegTree(int n, auto &&f, T val = {}) : n(n), f(f), nodes(2 * n, val) {}
+  T full() const { return _node(1); }           // O(1)
+  T get(int i) const { return _node(i + n); }   // O(1)
+  T &operator[](int i) { return nodes[i + n]; } // O(1)
+  T query(int l, int r) const { return _check(l, r), _query(l + n, r + n); }
+  void update(int i, bool single) { _check(i, i), _build(i + n, single); }
+  void _build(int i, bool single) { // O(log n) / [0, i] O(n)
+    function<void()> dec[] = {[&]() { i--; }, [&]() { i >>= 1; }};
+    for (i >>= 1; i > 0; dec[single]()) {
+      _merge(i);
+    }
+  }
+  T _query(int l, int r) const { // [l, r] O(log n)
+    return l == r   ? _node(l)
+           : l & 1  ? f(_node(l), _query(l + 1, r))
+           : ~r & 1 ? f(_query(l, r - 1), _node(r))
+                    : _query(l >> 1, r >> 1);
+  }
+  virtual T _node(int i) const { return nodes[i]; }
+  void _merge(int i) { nodes[i] = f(_node(i << 1), _node(i << 1 | 1)); }
+  void _check(int l, int r) const { assert(l >= 0 && l <= r && r < n); }
+};
+
+struct Seg : array<Mint, 5> {
+  // non-commutative: left vs right segment
+  Seg join(const Seg &rhs) const {
+    auto ans = *this;
+    for (int i = 0; i < 5; i++) {
+      ans[i] += rhs[i];
+    }
+    ans[4] += (*this)[0] * rhs[3] + (*this)[1] * rhs[2];
+    return ans;
+  }
+};
+
+void solve(int t) {
+  String s;
+  int n = s.size();
+  Pow2 pow2(n);
+  Int q;
+  vector<Int> v(q);
+  vector<int> ans(q);
+  SegTree<Seg> segtree(bit_ceil<unsigned>(n), &Seg::join);
+  auto f = [&](int i) {
+    return s[i] & 1 ? Seg{pow2[i], 0, pow2[n - i - 1], 0, 0}
+                    : Seg{0, pow2[i], 0, pow2[n - i - 1], 0};
+  };
+  for (int i = 0; i < n; i++) { // O(n)
+    segtree[i] = f(i);
+  }
+  segtree.update(n - 1, false); // O(n)
+  for (int i = 0; i < q; i++) { // O(q*log n)
+    auto j = v[i] - 1;
+    s[j] ^= 1; // toggle bit
+    segtree[j] = f(j);
+    segtree.update(j, true);
+    ans[i] = segtree.full()[4] + pow2[n] - 1;
+  }
+  println(ans);
+}
+
+int main() {
+  cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);
+  Int t;
+  for (int i = 1; i <= t; i++) {
+    solve(i);
+  }
+}

509febc4a737c0a988e06b993010a25c65d70a3f
Diego Sogari <diego.sogari@gmail.com>
2025-02-12 22:28:00 -0300

    [Playground] Add shelf output to bookcase problem

--- playground/bookcase/main.cc
+++ playground/bookcase/main.cc
@@ -13,13 +13,15 @@ using namespace std;
 init();
 #endif
 
-int len1(auto &&b, int n, int k, auto &&seq) { // O({n-k,k})
+int len1(auto &&b, int n, int k, auto &&seq, auto &&shelves) { // O({n-k,k})
   if (k == 1) {
     return n;
   }
+  vector<int> shf(n);
   vector<bool> vis(n);
-  for (auto i : seq) { // O(n)
-    vis[i] = true;
+  for (int j = 0; j < k; j++) { // O(k)
+    vis[seq[j]] = true;
+    shf[seq[j]] = j + 1;
   }
   vector<vector<int>> good(n);
   for (int i = 0; i < n; i++) { // O(n*k)
@@ -37,6 +39,12 @@ int len1(auto &&b, int n, int k, auto &&seq) { // O({n-k,k})
   int ans = INT_MAX;
   // int pass1 = 0, pass2 = 0, pass3 = 0;
   vector<int> top(k), cnt(k);
+  auto save = [&](int i, int j) -> array<int, 3> {
+    return {exchange(top[j], b[i][1]), exchange(shf[i], j + 1), cnt[j]++};
+  };
+  auto restore = [&](int i, int j, const array<int, 3> &saved) {
+    top[j] = saved[0], shf[i] = saved[1], cnt[j] = saved[2];
+  };
   auto f = [&](auto &self, int i, int mx) -> void { // O({n-k,k})
     if (mx >= ans) {
       // pass1++;
@@ -45,6 +53,7 @@ int len1(auto &&b, int n, int k, auto &&seq) { // O({n-k,k})
     if (i == n) {
       // pass2++;
       ans = mx;
+      shelves = shf;
       return;
     }
     if (vis[i]) {
@@ -53,10 +62,9 @@ int len1(auto &&b, int n, int k, auto &&seq) { // O({n-k,k})
     // pass3++;
     for (auto j : good[i]) {
       if (b[i][1] >= top[j]) {
-        auto saved = top[j];
-        top[j] = b[i][1], cnt[j]++;
+        auto saved = save(i, j);
         self(self, i + 1, max(mx, cnt[j]));
-        top[j] = saved, cnt[j]--; // backtrack
+        restore(i, j, saved); // backtrack
       }
     }
   };
@@ -79,10 +87,11 @@ void solve(int t) {
   auto seq = lis(cmp, 0, n);
   int k = seq.size(); // O(n*log n)
   auto t0 = now();
-  int l1 = len1(b, n, k, seq);
+  vector<int> shelves;
+  int l1 = len1(b, n, k, seq, shelves);
   auto t1 = now();
   chrono::duration<double, milli> d1 = t1 - t0;
-  println(b, seq, k, l1, d1.count());
+  println(b, seq, shelves, k, l1, d1.count());
 }
 
 int main() {

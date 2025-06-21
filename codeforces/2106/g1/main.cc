8f5d79b0fdf53fc83f058d1cb3f5457e527304c1
Diego Sogari <diego.sogari@gmail.com>
2025-06-21 16:02:09 -0300

    [2106] Clean up code

--- codeforces/2106/g1/main.cc
+++ codeforces/2106/g1/main.cc
@@ -1,6 +1,4 @@
 /**
- * https://codeforces.com/contest/2106/submission/325496281
- *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -25,33 +23,9 @@ template <typename T> struct Number {
   operator T() const { return x; }
 };
 
-template <typename T> struct String : basic_string<T> {
-  using basic_string<T>::basic_string;
-  String() { cin >> *this; }
-};
-
 using Int = Number<int>;
-using Str = String<char>;
 
-void solve(int t) {
-  Int n;
-  Str s;
-  i64 ans = 0, top = 0, bot = 0;
-  for (int i = 0; i < n; i++) {
-    if (i > 0 && s[i - 1] == '1') {
-      bot = 0;
-    }
-    if (s[i] == '0') {
-      top += i;
-      bot += n - i - 1;
-    } else {
-      bot++;
-      top = bot;
-    }
-    ans = max({ans, top, bot});
-  }
-  println(ans);
-}
+void solve(int t) {}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

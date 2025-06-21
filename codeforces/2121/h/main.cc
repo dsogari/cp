03902c962932f5afb044a0e90b9cc262fe97b11c
Diego Sogari <diego.sogari@gmail.com>
2025-06-21 12:56:48 -0300

    [2121] Clean up code

--- codeforces/2121/h/main.cc
+++ codeforces/2121/h/main.cc
@@ -1,12 +1,11 @@
 /**
- * https://codeforces.com/contest/2121/submission/325463307
+ * https://codeforces.com/contest/2121/submission/325465683
  *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
-using u64 = uint64_t;
 
 #ifdef ONLINE_JUDGE
 #define debug(...)
@@ -30,53 +29,18 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-struct SplitHash {
-  size_t operator()(u64 x) const {
-    x += (u64)this + 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
-};
-
-template <typename T> struct SparseFenTree {
-  int n;
-  unordered_map<int, T, SplitHash> nodes;
-  SparseFenTree(int n, T val = {}) : n(n), nodes{{0, val}} {}
-  T query(int i) const { // O(log n)
-    assert(i < n);
-    T ans = nodes.find(0)->second;
-    for (i++; i > 0; i -= i & -i) {
-      auto it = nodes.find(i);
-      if (it != nodes.end()) {
-        ans += it->second;
-      }
-    }
-    return ans;
-  }
-  void update(int i, const T &val) { // O(log n)
-    assert(i >= 0);
-    for (i++; i <= n; i += i & -i) nodes[i] += val;
-  }
-};
-
 void solve(int t) {
   Int n;
   vector<array<Int, 2>> a(n);
-  SparseFenTree<int> fen(1e9 + 1);
-  set<int> seen;
+  multiset<int> seen;
   vector<int> ans;
   for (auto [l, r] : a) {
-    fen.update(l, 1);
     seen.insert(l);
     auto it = seen.upper_bound(r);
     if (it != seen.end()) {
-      fen.update(*it, -1);
-      if (fen.query(r) == fen.query(*it)) {
-        seen.erase(it);
-      }
+      seen.erase(it);
     }
-    ans.push_back(fen.query(1e9));
+    ans.push_back(seen.size());
   }
   println(ans);
 }

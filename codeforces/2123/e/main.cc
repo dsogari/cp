bbee7b0599db61155482b155c45ba18a36c4b0e2
Diego Sogari <diego.sogari@gmail.com>
2025-07-01 17:16:25 -0300

    [2123] Solve problem E

--- codeforces/2123/e/main.cc
+++ codeforces/2123/e/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2123/submission/326997339
+ *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -12,6 +14,9 @@ using namespace std;
 init();
 #endif
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &a) {
+  return ranges::for_each(a, [&os](auto &ai) { os << ai << ' '; }), os;
+}
 void println(auto &&...args) { ((cout << args << ' '), ...) << endl; }
 
 template <typename T> struct Number {
@@ -24,7 +29,48 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-void solve(int t) {}
+template <typename T> struct FenTree {
+  int n;
+  vector<T> nodes;
+  FenTree(int n, T val = {}) : n(n), nodes(n + 1, val) {}
+  T query(int i) const { // O(log n)
+    assert(i < n);
+    T ans = nodes[0];
+    for (i++; i > 0; i -= i & -i) ans += nodes[i];
+    return ans;
+  }
+  void update(int i, const T &val) { // O(log n)
+    assert(i >= 0);
+    for (i++; i <= n; i += i & -i) nodes[i] += val;
+  }
+};
+
+void solve(int t) {
+  Int n;
+  vector<Int> a(n);
+  vector<int> pref(n + 1), freq(n + 1), ans(n + 1, 1);
+  FenTree<int> fen(n + 1);
+  for (auto &&x : a) {
+    pref[x] = 1;
+    freq[x]++;
+  }
+  int mex = n;
+  for (int i = 0, j = n - 1; i < n; i++, j--) {
+    if (freq[j] == 0) {
+      mex = j;
+    }
+    pref[i + 1] += pref[i];
+  }
+  for (int i = 0, k = n - 1; k > 0; k--) {
+    while (i < mex && n - pref[i] + 1 >= k) {
+      assert(freq[i] > 0);
+      fen.update(freq[i], 1);
+      i++;
+    }
+    ans[k] = fen.query(k) + (n - pref[mex] >= k);
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

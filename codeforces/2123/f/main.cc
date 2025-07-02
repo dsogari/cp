0ef224150081fcbf2ac766aadf40e3f4376d552f
Diego Sogari <diego.sogari@gmail.com>
2025-07-02 06:56:36 -0300

    [2123] Solve problem F

--- codeforces/2123/f/main.cc
+++ codeforces/2123/f/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2123/submission/327062166
+ *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -12,6 +14,9 @@ using namespace std;
 init();
 #endif
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &a) {
+  return ranges::for_each(a, [&os](auto &ai) { os << ai << ' '; }), os;
+}
 void println(auto &&...args) { ((cout << args << ' '), ...) << endl; }
 
 template <typename T> struct Number {
@@ -24,7 +29,37 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-void solve(int t) {}
+struct Sieve : vector<int> {
+  Sieve(int n, bool isval) { // max value or min count
+    int mx = isval ? n : ceil(n * log(n * log(n + 1) + 1) + 1);
+    vector<bool> vis(mx + 1);
+    for (int i = 2; i <= mx; i++) { // O(mx*log log mx)
+      if (!vis[i]) {
+        push_back(i);
+        for (int j = 2 * i; j <= mx; j += i) {
+          vis[j] = true;
+        }
+      }
+    }
+  }
+};
+
+void solve(int t) {
+  Int n;
+  vector<int> ans(n);
+  vector<bool> vis(n + 1);
+  Sieve primes(n, true);                        // O(n*log log n)
+  iota(ans.begin(), ans.end(), 1);              // O(n)
+  for (auto p : ranges::reverse_view(primes)) { // O(n)
+    for (int x = p, y = x; x <= n; x += p) {
+      if (!vis[x]) {
+        swap(ans[x - 1], ans[y - 1]);
+        vis[y = x] = true;
+      }
+    }
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

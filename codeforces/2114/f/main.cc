d06de89876aa1bcaf1d02461acbedafc96ef0116
Diego Sogari <diego.sogari@gmail.com>
2025-07-03 06:02:16 -0300

    [2114] Clean up code

--- codeforces/2114/f/main.cc
+++ codeforces/2114/f/main.cc
@@ -1,5 +1,5 @@
 /**
- * https://codeforces.com/contest/2114/submission/327195973
+ * https://codeforces.com/contest/2114/submission/327203262
  *
  * (c) 2025 Diego Sogari
  */
@@ -26,65 +26,32 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-struct Sieve : vector<int> {
-  Sieve(int n, bool isval) { // max value or min count
-    int mx = isval ? n : ceil(n * log(n * log(n + 1) + 1) + 1);
-    vector<bool> vis(mx + 1);
-    for (int i = 2; i <= mx; i++) { // O(mx*log log mx)
-      if (!vis[i]) {
-        push_back(i);
-        for (int j = 2 * i; j <= mx; j += i) {
-          vis[j] = true;
-        }
-      }
-    }
-  }
-} sieve(1e6, true);
-
-struct PrimeFactors : vector<int> {
-  PrimeFactors(const auto &primes, int n) { // O(log log n)
-    for (auto &&p : primes) {
-      if (p > n) {
-        break;
-      } else if (n % p == 0) {
-        push_back(p);
-      }
-    }
-  }
-};
-
-map<array<int, 3>, int> memo;
-
 void solve(int t) {
   Int x, y, k;
   auto f = [&](int x) { // O(n)
     if (x == 1) {
       return 0;
     }
-    if (x <= k) {
-      return 1;
-    }
-    map<int, int> dp;
-    for (int i = 2; i <= k && i * i <= x; i++) { // O(sqrt n)
+    vector<int> divs;
+    for (int i = 1; i * i <= x; i++) { // O(sqrt n)
       if (x % i == 0) {
-        dp.emplace(i, 1);
-        if (x / i <= k) {
-          dp.emplace(x / i, 1);
+        divs.push_back(i);
+        divs.push_back(x / i);
+      }
+    }
+    ranges::sort(divs);
+    auto m = divs.size();
+    vector<int> dp(m, m + 1);
+    dp[0] = 0;
+    for (int i = 1; i < m; i++) { // O(n)
+      for (int j = 0; j < i; j++) {
+        auto d = div(divs[i], divs[j]);
+        if (d.rem == 0 && d.quot <= k) {
+          dp[i] = min(dp[i], dp[j] + 1);
         }
       }
     }
-    for (auto &&[d1, c1] : dp) { // O(n)
-      for (auto &&[d2, c2] : dp) {
-        if (d2 >= d1 && d2 < x) {
-          auto y = d1 * d2;
-          if (y > k && x % y == 0) {
-            auto &c = dp[y];
-            c = min(c ? c : INT_MAX, c1 + c2);
-          }
-        }
-      }
-    }
-    return dp.contains(x) ? dp[x] : -1;
+    return dp.back() > m ? -1 : dp.back();
   };
   auto g = gcd(+x, +y);
   auto a = f(x / g);

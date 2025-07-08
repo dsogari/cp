d91ed00388ccbf7ca5e4e7ca4f164760a4bc5fa8
Diego Sogari <diego.sogari@gmail.com>
2025-07-08 09:36:14 -0300

    [2091] Solve problem G

--- codeforces/2091/g/main.cc
+++ codeforces/2091/g/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/2091/submission/328088238
+ *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -24,7 +26,33 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-void solve(int t) {}
+void solve(int t) {
+  Int s, k;
+  queue<array<int, 3>> q;
+  set<array<int, 3>> vis;
+  q.push({0, k, 0});
+  while (q.size()) {
+    auto [l, k, m] = q.front();
+    q.pop();
+    if (k <= 1) {
+      return println(1);
+    }
+    auto d = div(s - l, k);
+    if (d.rem <= m) {
+      return println(k);
+    }
+    for (int i = 0; i <= m; i++, l++) {
+      auto d = div(s - l, k);
+      int cnt = 2 * (d.quot - 1) + 1; // available remainders mod k-2
+      int inc = (s - l - d.rem) / (k - 1) < k - 1; // whether cannot return to 0
+      int inc2 = l + inc >= k - 1;                 // whether can go back past 0
+      array<int, 3> next = {(l + inc) % (k - 1), k - 2, cnt - inc + inc2};
+      if (vis.insert(next).second) {
+        q.push(next);
+      }
+    }
+  }
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

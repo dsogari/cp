445a65a3017ce2942bd615fb790586e347043d43
Diego Sogari <diego.sogari@gmail.com>
2025-07-17 19:54:50 -0300

    [2126] Solve problem F

--- codeforces/2126/f/main.cc
+++ codeforces/2126/f/main.cc
@@ -1,9 +1,12 @@
 /**
+ * https://codeforces.com/contest/2126/submission/329554360
+ *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = int64_t;
 
 #ifdef ONLINE_JUDGE
 #define debug(...)
@@ -24,7 +27,60 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-void solve(int t) {}
+struct WGraph : vector<vector<array<int, 2>>> {
+  const int n, m;
+  vector<array<Int, 3>> edges;
+  WGraph(int n, int m = 0)
+      : vector<vector<array<int, 2>>>(n + 1), n(n), m(m), edges(m) {
+    for (auto &[u, v, w] : edges) {
+      add(u, v, w);
+    }
+  }
+  void add(int u, int v, int w) {
+    (*this)[u].push_back({v, w}), (*this)[v].push_back({u, w});
+  }
+};
+
+void solve(int t) {
+  Int n, q;
+  vector<Int> a(n);
+  WGraph g(n, n - 1);
+  vector<array<Int, 2>> b(q);
+  map<array<int, 2>, i64> memo;
+  vector<array<int, 2>> par(n + 1);
+  i64 ans = 0;
+  auto dfs = [&](auto &dfs, int u, int p) -> void {
+    for (auto &&[v, c] : g[u]) {
+      if (v != p) {
+        par[v] = {u, c};
+        memo[{u, a[v - 1]}] += c;
+        if (a[u - 1] != a[v - 1]) {
+          ans += c;
+        }
+        dfs(dfs, v, u);
+      }
+    }
+  };
+  dfs(dfs, 1, 1);
+  for (auto &&[v, x] : b) {
+    if (a[v - 1] != x) {
+      auto [p, c] = par[v];
+      if (p) {
+        if (a[p - 1] == a[v - 1]) {
+          ans += c;
+        }
+        if (a[p - 1] == x) {
+          ans -= c;
+        }
+        memo[{p, a[v - 1]}] -= c;
+        memo[{p, x}] += c;
+      }
+      ans += memo[{v, a[v - 1]}] - memo[{v, x}];
+      a[v - 1] = x;
+    }
+    println(ans);
+  }
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);

288c71f08f409cb9505125e3628336fc70ebb403
Diego Sogari <diego.sogari@gmail.com>
2025-07-19 09:41:25 -0300

    [2126] Solve problem G

--- codeforces/2126/g/main.cc
+++ codeforces/2126/g/main.cc
@@ -1,4 +1,7 @@
 /**
+ * https://codeforces.com/contest/2126/submission/329775293
+ * https://codeforces.com/contest/2126/submission/329775391
+ *
  * (c) 2025 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -24,7 +27,39 @@ template <typename T> struct Number {
 
 using Int = Number<int>;
 
-void solve(int t) {}
+struct Iota : vector<int> {
+  Iota(int n, int s = 0) : vector<int>(n) { iota(begin(), end(), s); }
+  Iota(int n, auto &&f, int s = 0) : Iota(n, s) { ranges::sort(*this, f); }
+};
+
+struct PaintDSU {
+  vector<int> par;
+  PaintDSU(int n) : par(n) { iota(par.begin(), par.end(), 0); }
+  int find(int u) { return u == par[u] ? u : par[u] = find(par[u]); } // O(lg n)
+  int merge(int u, int p) { return par[find(u)] = find(p); } // [u->p] O(lg n)
+};
+
+void solve(int t) {
+  Int n;
+  vector<Int> a(n);
+  PaintDSU left(n), right(n);
+  Iota idx(n, [&](int i, int j) { return a[i] > a[j]; });
+  int ans = 0;
+  auto f = [&](auto &dsu, int i, int inc, int end) {
+    int p = dsu.find(i);
+    ans = max(ans, a[i] - a[p]);
+    if (p != end) {
+      dsu.merge(p, p + inc);
+    }
+  };
+  for (auto &&i : idx) {
+    f(left, i, -1, 0);
+    f(left, i, -1, 0);
+    f(right, i, 1, n - 1);
+    f(right, i, 1, n - 1);
+  }
+  println(ans);
+}
 
 int main() {
   cin.tie(nullptr)->tie(nullptr)->sync_with_stdio(false);
